6b60f5d417199d74a7edd8004182b67654a56946
abrordc <moh.abrorilhuda@gmail.com>
2024-09-13 10:42:32 +0700

    update: fitur guru & add library & cretae README

---
+++ README.md
@@ -0,0 +1,11 @@
+# TEMPLATE web
+technology 
+* [bootstrap v 5.3](https://getbootstrap.com/)
+* [boostrap icons v 1.11.3](https://icons.getbootstrap.com/)
+* [jquery v 3.7.1](https://jquery.com/) 
+* [typed.js v 2.1.0](https://github.com/mattboldt/typed.js/) 
+* [animate.css v 4.1.1](https://github.com/animate-css/animate.css) 
+
+```git 
+git clone https://github.com/abrordc/Template-Web.git
+```
--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,85 @@
+// js native code
+// message : capek pakek library jquery full screen makanya pakek js native aja
+function toggleFullscreen() {
+    const elem = document.documentElement;
+    // condition full screen with all browser
+    if (!document.fullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        } else if (elem.mozRequestFullscreen) {
+            elem.mozRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullscreen) {
+            document.mozCancelFullscreen();
+        }
+    }
+
+    // swicth icons full screen
+    const button = document.getElementById("toggleFullscreen");
+    const icon = button.querySelector("i");
+
+    if (document.fullscreenElement) {
+        icon.classList.add("bi-fullscreen");
+        icon.classList.remove("bi-fullscreen-exit");
+    } else {
+        icon.classList.add("bi-fullscreen-exit");
+        icon.classList.remove("bi-fullscreen");
+    }
+}
+
+// jquery code
 (function ($) {
+    // handling text to typing with type.js
     const mengetik = new Typed(".type", {
         strings: ["Selamat Datang Di Situs Resmi", "Media Informasi"],
         typeSpeed: 60,
         backSpeed: 50,
         loop: true
     });
+    // handling button back to top
+    // message: add animate css back-to-top
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
-            $(".back-to-top").fadeIn("slow");
+            $(".back-to-top")
+                .fadeIn("slow")
+                .addClass("animate__bounceInUp")
+                .removeClass("animate__bounceOutUp");
         } else {
-            $(".back-to-top").fadeOut("slow");
+            $(".back-to-top")
+                .fadeOut("slow")
+                .addClass("animate__bounceOutUp")
+                .removeClass("animate__bounceInUp");
         }
     });
     $(".back-to-top").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
         return false;
+    });
+
+    // handling button full screen animate from animated.css
+    var isScrolling;
+    $(window).scroll(function () {
+        $("#toggleFullscreen")
+            .removeClass("animate__fadeInTopLeft")
+            .addClass("animate__bounceOutLeft")
+            .fadeOut(600, function () {
+                $(this).removeClass("animate__bounceOutLeft");
+            });
+
+        clearTimeout(isScrolling);
+        isScrolling = setTimeout(function () {
+            $("#toggleFullscreen").show().addClass("animate__fadeInTopLeft");
+        }, 1000);
     });
 })(jQuery);
