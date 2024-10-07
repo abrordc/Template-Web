8709f788e6b480000dcba3cef3f9b7bf94d7a0a8
abrordc <moh.abrorilhuda@gmail.com>
2024-10-07 17:13:40 +0800

    add: kegiatan.html ,disable inspect,update css

--- src/js/script.js
+++ src/js/script.js
@@ -1,85 +1,97 @@
 // js native code
 // message : capek pakek library jquery full screen makanya pakek js native aja
 function toggleFullscreen() {
-    const elem = document.documentElement;
-    // condition full screen with all browser
-    if (!document.fullscreenElement) {
-        if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) {
-            elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) {
-            elem.msRequestFullscreen();
-        } else if (elem.mozRequestFullscreen) {
-            elem.mozRequestFullscreen();
-        }
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        } else if (document.mozCancelFullscreen) {
-            document.mozCancelFullscreen();
-        }
+  const elem = document.documentElement;
+  // condition full screen with all browser
+  if (!document.fullscreenElement) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullscreen) {
+      elem.mozRequestFullscreen();
     }
-
-    // swicth icons full screen
-    const button = document.getElementById("toggleFullscreen");
-    const icon = button.querySelector("i");
-
-    if (document.fullscreenElement) {
-        icon.classList.add("bi-fullscreen");
-        icon.classList.remove("bi-fullscreen-exit");
-    } else {
-        icon.classList.add("bi-fullscreen-exit");
-        icon.classList.remove("bi-fullscreen");
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+      document.mozCancelFullscreen();
     }
+  }
+
+  // swicth icons full screen
+  const button = document.getElementById("toggleFullscreen");
+  const icon = button.querySelector("i");
+
+  if (document.fullscreenElement) {
+    icon.classList.add("bi-fullscreen");
+    icon.classList.remove("bi-fullscreen-exit");
+  } else {
+    icon.classList.add("bi-fullscreen-exit");
+    icon.classList.remove("bi-fullscreen");
+  }
 }
 
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+
+  for (let i = 0; i < e.target.children.length; i++) {
+    const menuItem = e.target.children[i];
+    if (menuItem.textContent.toLowerCase().includes("inspect")) {
+      menuItem.addEventListener("click", function (e) {
+        e.preventDefault();
+      });
+    }
+  }
+});
 // jquery code
 (function ($) {
-    // handling text to typing with type.js
-    const mengetik = new Typed(".type", {
-        strings: ["Selamat Datang Di Situs Resmi", "Media Informasi"],
-        typeSpeed: 60,
-        backSpeed: 50,
-        loop: true
-    });
-    // handling button back to top
-    // message: add animate css back-to-top
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $(".back-to-top")
-                .fadeIn("slow")
-                .addClass("animate__bounceInUp")
-                .removeClass("animate__bounceOutUp");
-        } else {
-            $(".back-to-top")
-                .fadeOut("slow")
-                .addClass("animate__bounceOutUp")
-                .removeClass("animate__bounceInUp");
-        }
-    });
-    $(".back-to-top").click(function () {
-        $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-        return false;
-    });
+  // handling text to typing with type.js
+  const mengetik = new Typed(".type", {
+    strings: ["Selamat Datang Di Situs Resmi", "Media Informasi"],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: true,
+  });
+  // handling button back to top
+  // message: add animate css back-to-top
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top")
+        .fadeIn("slow")
+        .addClass("animate__bounceInUp")
+        .removeClass("animate__bounceOutUp");
+    } else {
+      $(".back-to-top")
+        .fadeOut("slow")
+        .addClass("animate__bounceOutUp")
+        .removeClass("animate__bounceInUp");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // handling button full screen animate from animated.css
-    var isScrolling;
-    $(window).scroll(function () {
-        $("#toggleFullscreen")
-            .removeClass("animate__fadeInTopLeft")
-            .addClass("animate__bounceOutLeft")
-            .fadeOut(600, function () {
-                $(this).removeClass("animate__bounceOutLeft");
-            });
+  // handling button full screen animate from animated.css
+  var isScrolling;
+  $(window).scroll(function () {
+    $("#toggleFullscreen")
+      .removeClass("animate__fadeInTopLeft")
+      .addClass("animate__bounceOutLeft")
+      .fadeOut(600, function () {
+        $(this).removeClass("animate__bounceOutLeft");
+      });
 
-        clearTimeout(isScrolling);
-        isScrolling = setTimeout(function () {
-            $("#toggleFullscreen").show().addClass("animate__fadeInTopLeft");
-        }, 1000);
-    });
+    clearTimeout(isScrolling);
+    isScrolling = setTimeout(function () {
+      $("#toggleFullscreen").show().addClass("animate__fadeInTopLeft");
+    }, 1000);
+  });
 })(jQuery);
